6217654f6d2b9a176e8488b9f6ee48c45de16d11
rodrigmzm <103709885+rodrigmzm@users.noreply.github.com>
2022-11-22 16:34:30 -0600

    Add files via upload

---
+++ Proyecto/src/proyecto/Logueo.java
@@ -0,0 +1,55 @@
+package proyecto;
+import javax.swing.JOptionPane;
+
+// @author Rodrigo
+
+public class Logueo {
+    private int opcion;
+    private String u, c, usuario, clave;
+   
+    public Logueo() {
+	this.opcion = 0;
+	this.u = "";
+	this.c = "";
+	this.usuario = ".";
+	this.clave = ".";
+    }
+
+    public Logueo(int opcion, String u, String c, String usuario, String clave) {
+	this.opcion = opcion;
+	this.u = u;
+	this.c = c;
+	this.usuario = usuario;
+	this.clave = clave;
+    }
+    
+    public void loguear(){
+
+	while(opcion != 1){
+	    opcion = Integer.parseInt(JOptionPane.showInputDialog
+	    ("¿Cuenta con usuario y clave para iniciar sesión?"
+		+ "\n[1] Si \n[2] No"));
+	    if (opcion == 2) {
+		u = JOptionPane.showInputDialog("Usuario: ");
+		c = JOptionPane.showInputDialog("Contraseña: ");
+		JOptionPane.showMessageDialog(null,
+			"Usuario: "+u+"\nContraseña: "+c+"\n Ahora inicie sesion");
+		
+	    } else if (opcion == 1) {
+		    while(!usuario.equals(u) || !clave.equals(c)){
+			usuario = JOptionPane.showInputDialog("Usuario: ");
+			clave = JOptionPane.showInputDialog("Clave: ");
+			if (usuario.equals(u) && clave.equals(c)) {
+			    JOptionPane.showMessageDialog(null,
+				    "Bienvenido, "+usuario);
+			} else if (!usuario.equals(u) || !clave.equals(c)) {
+			    JOptionPane.showMessageDialog(null,
+				    "Los datos no coinciden. \nIntente de nuevo");
+			}
+		    }
+		    
+		}
+	}
+    }
+    
+}
---
+++ Proyecto/src/proyecto/Main.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+// @author Rodrigo
+public class Main {
+
+    public static void main(String[] args) {
+        Logueo l = new Logueo();
+	l.loguear();
+    }
+
+}
